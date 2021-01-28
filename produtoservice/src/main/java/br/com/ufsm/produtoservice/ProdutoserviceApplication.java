12dc9932477d779404ca7ff8ebdb84b444a7cdae
Pedro Carara <cararax@gmail.com>
2021-01-28 15:40:14 -0300

    commit inicial, adicionei dependencias web, data sql, h2, postgresql, devtools e validation

---
+++ produtoservice/src/main/java/br/com/ufsm/produtoservice/ProdutoserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufsm.produtoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoserviceApplication.class, args);
+	}
+
+}
---
+++ produtoservice/src/test/java/br/com/ufsm/produtoservice/ProdutoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ufsm.produtoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
